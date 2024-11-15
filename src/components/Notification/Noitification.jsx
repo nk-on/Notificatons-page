ad4d27589bfb8f1f3b45d131a73db909f2daed30
nk-on <johndoe@example.com>
2024-11-15 22:56:39 +0400

    Feat:adding background color and red dot notifications which are not read

--- src/components/Notification/Noitification.jsx
+++ src/components/Notification/Noitification.jsx
@@ -1,15 +1,25 @@
-import './Notification.css'
-export default function Notification({name,notificationText, timePassed ,isRead,picture}) {
-  return (<div className='notification-container' style={{backgroundColor: isRead ? '#FFF':'#F7FAFD'}}>
-    <div className="main-txt">
-    
-    <img src={picture} className='profile-image' />
-      <h1>{name}</h1>
-      <p>{notificationText}</p>
-      {!isRead && <div className='dot'></div>}
+import './Notification.css';
+export default function Notification({
+  name,
+  notificationText,
+  timePassed,
+  isRead,
+  picture,
+}) {
+  return (
+    <div
+      className="notification-container"
+      style={{ backgroundColor: isRead ? '#FFF' : '#F7FAFD' }}
+    >
+      <div className="main-txt">
+        <img src={picture} className="profile-image" />
+
+        <h1>{name}</h1>
+        <p>{notificationText}</p>
+
+        {!isRead && <div className="dot"></div>}
+      </div>
+      <div className="time-passed">{timePassed}</div>
     </div>
-    <div className="time-passed">
-      {timePassed}
-    </div>
-  </div>);
+  );
 }
