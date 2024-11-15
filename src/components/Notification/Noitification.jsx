90f7f08a4708091d4be6506ca05a6aa87e0b68ff
nk-on <2nkoz@proton.me>
2024-11-14 23:21:27 -0800

    Adding notification components

---
+++ src/components/Notification/Noitification.jsx
@@ -0,0 +1,12 @@
+import './notification.css'
+export default function Notification() {
+  return (<div className='notification-container'>
+    <div className="main-txt">
+      <h1>Mark webber</h1>
+      <p>Followed you</p>
+    </div>
+    <div className="time-passed">
+      5m ago
+        </div>
+  </div>);
+}
