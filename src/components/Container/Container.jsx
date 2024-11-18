25baece5f25c4ab871adbf5c95988ca215c44bdd
nk-on <johndoe@example.com>
2024-11-18 23:31:13 +0400

    Moving notifications Aray to separete file

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,67 +1,9 @@
 import './Container.css';
 import Notification from '../Notification/Noitification';
 import { useState } from 'react';
+import { notificationsArr } from './notifictions';
 export default function Container() {
-  const [notifications, setNotificastions] = useState([
-    {
-      id: 1,
-      notificationText: 'You have a new message from John.',
-      isRead: false,
-      picture: 'src/assets/Image-1.png',
-      name: 'John',
-      timePassed: '2 hours ago',
-    },
-    {
-      id: 2,
-      notificationText: 'Emily liked your post.',
-      isRead: false,
-      picture: 'src/assets/Image-2.png',
-      name: 'Emily',
-      timePassed: '15 minutes ago',
-    },
-    {
-      id: 3,
-      notificationText: 'New comment from Michael on your photo.',
-      isRead: false,
-      picture: 'src/assets/Image-3.png',
-      name: 'Michael',
-      timePassed: '1 day ago',
-    },
-    {
-      id: 4,
-      notificationText: 'Sarah invited you to an event.',
-      isRead: true,
-      picture: 'src/assets/Image-4.png',
-      name: 'Sarah',
-      timePassed: '3 days ago',
-    },
-    {
-      id: 5,
-      notificationText: 'Alex sent you a friend request.',
-      isRead: true,
-      picture: 'src/assets/Image-5.png',
-      name: 'Alex',
-      timePassed: '5 hours ago',
-      message:
-        'Hello, thanks for setting up the Chess Club. I’ve been a member for a few weeks now and I’m already having lots of fun and improving my game.',
-    },
-    {
-      id: 6,
-      notificationText: 'Jessica mentioned you in a comment.',
-      isRead: true,
-      picture: 'src/assets/Image-6.png',
-      name: 'Jessica',
-      timePassed: '30 minutes ago',
-    },
-    {
-      id: 7,
-      notificationText: 'You have a new follower: Liam.',
-      isRead: true,
-      picture: 'src/assets/Image-7.png',
-      name: 'Liam',
-      timePassed: '12 hours ago',
-    },
-  ]);
+  const [notifications, setNotificastions] = useState(notificationsArr);
   return (
     <div className="container">
       <div className="notifications">
